3aab2354a0fc8192e9d9bbd64700d9f650fef81e
Dana-Nurse <143125347+Dana-Nurse@users.noreply.github.com>
2024-05-06 11:39:09 -0400

    Update food_tracker.sql

--- food_tracker.sql
+++ food_tracker.sql
@@ -3,12 +3,12 @@ CREATE TABLE food_entries (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     quantity INTEGER NOT NULL,
-    expiration_days INTEGER NOT NULL,
-    category_id INTEGER NOT NULL
+    expiration_weeks INTEGER NOT NULL,
+    expiration_months INTEGER NOT NULL
 );
 
 -- Example records for tests to run 
-INSERT INTO food_entries (name, quantity, expiration_days, category_id) VALUES ('dhal', 1, 1, 0);
-INSERT INTO food_entries (name, quantity, expiration_days, category_id) VALUES ('sandwiches', 5, 1, 0);
-INSERT INTO food_entries (name, quantity, expiration_days, category_id) VALUES ('onions', 10, 8, 2);
-INSERT INTO food_entries (name, quantity, expiration_days, category_id) VALUES ('yogurt', 6, 12, 3);
+INSERT INTO food_entries (name, quantity, expiration_weeks, expiration_months) VALUES ('dhal', 1, 1, 0);
+INSERT INTO food_entries (name, quantity, expiration_weeks, expiration_months) VALUES ('sandwiches', 5, 1, 0);
+INSERT INTO food_entries (name, quantity, expiration_weeks, expiration_months) VALUES ('onions', 10, 8, 0);
+INSERT INTO food_entries (name, quantity, expiration_weeks, expiration_months) VALUES ('yogurt', 6, 12, 3);
